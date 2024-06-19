8d8d27a0137b8259d3884f7c7f2fc2fedb70a0f4
Niklas Burchhardt <nburchha@1-A-16.42heilbronn.de>
2024-06-19 16:33:20 +0200

    added mouse movement

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: niklasburchhardt <niklasburchhardt@stud    +#+  +:+       +#+        */
+/*   By: nburchha <nburchha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 21:11:03 by nburchha          #+#    #+#             */
-/*   Updated: 2024/06/19 09:51:41 by niklasburch      ###   ########.fr       */
+/*   Updated: 2024/06/19 16:23:06 by nburchha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@
 # define MOVE_SPEED 3.0f
 # define ROTATE_SPEED 0.1f
 # define MAX_KEY 266
+# define BONUS 1
 
 # define FLOOR '0'
 # define WALL '1'
@@ -119,6 +120,7 @@ void		draw_circle(float coords[2], float radius, uint32_t color, mlx_image_t *im
 /*PLAYER*/
 void		dda_algo(t_data *data, float angle);
 void		movement(t_data *data);
+bool		wall_collision(t_data *data, const char direction, const char xy);
 
 
 /*HOOKS*/
