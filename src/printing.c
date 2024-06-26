5d2f4e3fa4ba8062ce7af2f5574264d61fab8582
Pascal Sanger <psanger@2-C-3.42heilbronn.de>
2024-06-26 17:53:26 +0200

    NORM formater main printing

--- src/printing.c
+++ src/printing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   printing.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: niklasburchhardt <niklasburchhardt@stud    +#+  +:+       +#+        */
+/*   By: psanger <psanger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 09:58:41 by niklasburch       #+#    #+#             */
-/*   Updated: 2024/06/06 09:58:49 by niklasburch      ###   ########.fr       */
+/*   Updated: 2024/06/26 17:52:20 by psanger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,26 @@
 
 void	print_data(t_data *data)
 {
+	uint32_t	color;
+	uint8_t		r;
+	uint8_t		g;
+	uint8_t		b;
+
 	printf("n_texture: %p\n", data->n_texture);
 	printf("s_texture: %p\n", data->s_texture);
 	printf("w_texture: %p\n", data->w_texture);
 	printf("e_texture: %p\n", data->e_texture);
-	uint32_t color = data->ceiling_color;
-	uint8_t r = (color >> 24) & 0xFF;
-	uint8_t g = (color >> 16) & 0xFF;
-	uint8_t b = (color >> 8) & 0xFF;
+	color = data->ceiling_color;
+	r = (color >> 24) & 0xFF;
+	g = (color >> 16) & 0xFF;
+	b = (color >> 8) & 0xFF;
 	printf("CEILING: Red: %u, Green: %u, Blue: %u\n", r, g, b);
 	color = data->floor_color;
 	r = (color >> 24) & 0xFF;
 	g = (color >> 16) & 0xFF;
 	b = (color >> 8) & 0xFF;
 	printf("FLOOR: Red: %u, Green: %u, Blue: %u\n", r, g, b);
-	//print map:
+	// print map:
 	for (int i = 0; i < data->map->height; i++)
 		printf(".%s.\n", data->map->map[i]);
 }
